c7975f9c67a76873192cd12201b8ae3d342409b7
Fredrik Andersson <fredrik.andersson1@chasacademy.se>
2024-11-21 13:50:39 +0100

    fetch

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hello News!");
+const apiKey = "kzvccXW2l1EAE3toa0N5GFkWFozAqtqv";
+fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${apiKey}`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`fel med response ${response.statusText}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
