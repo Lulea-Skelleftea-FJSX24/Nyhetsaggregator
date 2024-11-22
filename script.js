dbb4cd5d1ab3cb5aaf4cfc86d14ac5294d939be5
Sami A <alhalabisami@gmail.com>
2024-11-22 14:36:23 +0100

    added a header section with the most viewed news the last 7 days

--- script.js
+++ script.js
@@ -135,3 +135,51 @@ fetch(
   .catch((error) => {
     console.log(error);
   });
+
+  // Most viewed last 7 days
+  fetch(
+    `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${apiKey}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`fel med response ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+        console.log("Most viewed api: ", data);
+        heroLayout(data.results);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+function heroLayout(articles) {
+  if (!articles || articles.length < 2) return;
+
+  const article1Element = document.getElementById("article1");
+  const article2Element = document.getElementById("article2");
+
+  if (!article1Element || !article2Element) return;
+
+  const article1 = articles[0];
+  const imageUrl1 = article1.media && article1.media[0] && article1.media[0]['media-metadata'] ? article1.media[0]['media-metadata'][2].url : '';
+
+  article1Element.innerHTML = `
+    <h1>${article1.title}</h1>
+    <p>${article1.abstract}</p>
+    <img src="${imageUrl1}" alt="Bild av nyhet">
+    <a href="${article1.url}" target="_blank">Läs Mer Här</a>
+  `;
+
+  const article2 = articles[1];
+  const imageUrl2 = article2.media && article2.media[0] && article2.media[0]['media-metadata'] ? article2.media[0]['media-metadata'][2].url : '';
+
+  article2Element.innerHTML = `
+    <h1>${article2.title}</h1>
+    <p>${article2.abstract}</p>
+    <img src="${imageUrl2}" alt="Bild av nyhet">
+    <a href="${article2.url}" target="_blank">Läs Mer Här</a>
+  `;
+}
+
