bf9a0b1d139802e4aa039ce2bf151b366e340634
Sami A <alhalabisami@gmail.com>
2024-11-29 10:12:56 +0100

    added css to the x button

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello News!");
+function showError(message) {
+  const errorModal = document.getElementById('errorModal');
+  const errorMessage = document.getElementById('errorMessage');
+  errorMessage.textContent = message;
+  errorModal.style.display = 'block'; // Shows the module on the site
+}
+
+// Function to close the module
+function closeModal() {
+  const errorModal = document.getElementById('errorModal');
+  errorModal.style.display = 'none'; // Hides the module
+}
+
+// If user clicks on x the module closes
+document.getElementById('closeModal').addEventListener('click', closeModal);
+
+// Click outside of the module and it will close
+window.addEventListener('click', function(event) {
+  const errorModal = document.getElementById('errorModal');
+  if (event.target === errorModal) {
+    errorModal.style.display = 'none';
+  }
+});
